bc8bd37a0b24d7a89d1b61c529888e34f3e42e85
Henry Ohanga <ohanga.henry@gmail.com>
2021-01-29 03:22:42 +0300

    add clear button to reset app state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import './Header.scss';
 
-const Header = () => {
+const Header = ({ onClear }) => {
   return (
     <div className="d-flex Header">
       <h1 className="p-h1 tc-grey-100">Your Little Plan</h1>
+      <button className="p-btn--secondary" onClick={onClear}>
+        Clear
+      </button>
     </div>
   );
 };
