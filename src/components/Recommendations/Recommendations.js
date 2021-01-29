b2b88d7d89a84c015687ad730358c8965625a14f
Henry Ohanga <ohanga.henry@gmail.com>
2021-01-29 05:34:02 +0300

    add error message to recommendation view

--- src/components/Recommendations/Recommendations.js
+++ src/components/Recommendations/Recommendations.js
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'react';
+
 import Recommendation from '../Recommendation/Recommendation';
 
 const Recommendations = ({ recommendations, isFetching }) => {
+  const [hasNoRecommendations, setHasNoRecommendations] = useState(false);
+
+  useEffect(() => {
+    setHasNoRecommendations(() => !recommendations.length && !isFetching);
+  }, [recommendations, isFetching]);
+
   return (
     <div className="Recommendations">
-      <h2 className="p-h2">We got your recommendation</h2>
-      <p className="p-p">
-        Based on your answers, this is what makes sense for you and what you
-        should pay.{' '}
-      </p>
+      {hasNoRecommendations ? (
+        <div>
+          <h2 className="p-h2 tc-red-700">It must be terrible!</h2>
+          <p className="p-p">
+            Unfortunately, there was an issue getting a recommendation. Please
+            try again
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h2 className="p-h2 tc-green-700">We got your recommendation</h2>
+          <p className="p-p">
+            Based on your answers, this is what makes sense for you and what you
+            should pay.{' '}
+          </p>
+        </div>
+      )}
 
       {isFetching ? (
         <div className="d-flex Recommendations-spinner mt32">
